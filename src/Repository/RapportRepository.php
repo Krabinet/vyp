a1188093fcd043ff4f5ac109f72b6de1975ae21e
Puillet <aigledore22@gmail.com>
2018-11-12 11:46:04 +0100

    Explosion imminente

--- src/Repository/RapportRepository.php
+++ src/Repository/RapportRepository.php
@@ -10,8 +10,8 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  * @method Rapport|null find($id, $lockMode = null, $lockVersion = null)
  * @method Rapport|null findOneBy(array $criteria, array $orderBy = null)
  * @method Rapport[]    findAll()
- * @method Rapport[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  * @method Rapport[]    findAllOrder()
+ * @method Rapport[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class RapportRepository extends ServiceEntityRepository
 {
